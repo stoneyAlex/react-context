3326f8b5ac1c9134f1e94b512e1b077c8eadaf04
stoneyAlex <57102959+stoneyAlex@users.noreply.github.com>
2022-07-05 17:14:26 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: shimingxia
+ * @Date: 2022-05-25 15:05:47
+ * @LastEditors: shimingxia
+ * @LastEditTime: 2022-07-05 16:50:13
+ * @Description: 
+ */
+import ContextPage from './pages/ContextPage'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <ContextPage />
+    </div>
+  );
+}
