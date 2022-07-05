aa11c41b10c03059359da1918042f6b8c27e17c7
stoneyAlex <57102959+stoneyAlex@users.noreply.github.com>
2022-07-05 17:16:32 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: shimingxia
+ * @Date: 2022-06-06 10:25:38
+ * @LastEditors: shimingxia
+ * @LastEditTime: 2022-07-05 17:11:59
+ * @Description: 
+ */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
